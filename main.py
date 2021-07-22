0f52b63aa5f2aea893792b8e472b04f58f5e008a
Diego Ortiz <diegoortizmatajira@gmail.com>
2021-07-22 16:39:56 -0500

    Fix the main parameter passing from one function to the next

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from excel_loading import load_excel_file
 from processor import generate_skill
 from json_writing import write_json_skill
 
-input_data = load_excel_file('PlantillaQA.xlsx')
-skill_object = generate_skill(input_data)
+input_data, header_data = load_excel_file('PlantillaQA.xlsx')
+skill_object = generate_skill(input_data, header_data)
 write_json_skill(skill_object, 'output.json')
