31bfa11741f67e159a016bc4c21b5bea6a23a98f
Diego Ortiz <diegoortizmatajira@gmail.com>
2021-07-22 16:43:11 -0500

    Fix the Json writing code

--- json_writing.py
+++ json_writing.py
@@ -1,9 +1,7 @@
 import json
 from json_structure.skill import Skill
 
-def write_json_skill(skill: Skill, output_file: str):
-    #print(f'Saving skill to {output_file}')
-    #print(skill)
 
+def write_json_skill(skill: Skill, output_file: str):
     with open(output_file, "w", encoding="utf-8") as writeJsonfile:
-        json.dump(skill, writeJsonfile, indent=4, default=str)
+        json.dump(skill, writeJsonfile, default=lambda o: o.__dict__, indent=4)
