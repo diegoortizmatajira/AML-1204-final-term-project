0b2d2f702d8f36805262f1f7ff091d8e8283fe80
rhenrym <85691507+rhenrym@users.noreply.github.com>
2021-07-21 22:40:49 -0500

    Load Excel implementation

--- json_writing.py
+++ json_writing.py
@@ -1,3 +1,9 @@
-def write_json_skill(skill, output_file):
-    print(f'Saving skill to {output_file}')
-    print(skill)
+import json
+from json_structure.skill import Skill
+
+def write_json_skill(skill: Skill, output_file: str):
+    #print(f'Saving skill to {output_file}')
+    #print(skill)
+
+    with open(output_file, "w", encoding="utf-8") as writeJsonfile:
+        json.dump(skill, writeJsonfile, indent=4, default=str)
